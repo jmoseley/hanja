d556012ff5a5c93d01f5fed1cbfc2307e0f81057
Jeremy Moseley <jeremy@jeremymoseley.net>
2020-10-12 17:11:07 -0700

    Add helper for configuring dev.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,4 +5,8 @@ if vercel whoami 2>&1 | grep "The specified token is not valid" ; then
   vercel login
 fi
 
+if [ ! -f ../hanja=config.dev.json ]; then
+  yarn configure-dev
+fi
+
 yarn concurrently --kill-others "yarn:dev-*"
